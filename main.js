721fb2bb2f2e69b2d820121e3895014a9022b921
Ashour100 <interinstitute5@gmail.com>
2022-03-08 20:24:24 +0100

    Function update

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 let NumeriElement=document.querySelector("h2");
 let arrNumeri=[];
-let RndNum= () => Math.floor(Math.random() * 10);
+let RndNum= function(){
+    let blackList=[];
+    let temp;
+    let ver=0;
+    let rep=0;
+    while(rep==0){
+        temp=Math.floor(Math.random() * 9 + 1);
+        if(!(blackList.includes(temp))){
+            rep=1;
+            blackList.push(temp);
+            return temp;
+        }
+    }
+}
 for(let i=0;i<5;i++){
     arrNumeri[i]=RndNum();
     NumeriElement.innerHTML+=arrNumeri[i]+" ";
 }
+
+
 
 let arrVerifica=[];
 let counter=0;
